91d4ac7dc795b4454650ac322f5227b3f6ceb8a1
RomanKamko <r.kamko@mail.ru>
2022-12-27 13:43:35 +0400

    Выполнил задание 1 - 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        // task 1
+        System.out.println("Задание 1");
+        int age = 32;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетния");
+        }
 
+        // task 2
+        System.out.println("Задание 2");
+        int temperature = 10;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+
+        // task 3
+        System.out.println("Задание 3");
+        int speed = 60;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+        }
     }
 }
