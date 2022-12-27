c718b2381dd1835188a31076ab71d6f94d04168d
RomanKamko <r.kamko@mail.ru>
2022-12-27 16:47:08 +0400

    Выполнил задание 1 - 6

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,84 @@ public class Main {
         System.out.println("Задание 1");
         int age = 32;
         if (age >= 18) {
-            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
         } else {
-            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетния");
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетния.");
         }
 
         // task 2
         System.out.println("Задание 2");
         int temperature = 10;
         if (temperature < 5) {
-            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
         } else {
-            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
         }
 
         // task 3
         System.out.println("Задание 3");
         int speed = 60;
         if (speed > 60) {
-            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф.");
         } else {
-            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно.");
+        }
+
+        // task 4
+        System.out.println("Задание 4");
+        int personAge = 11;
+        if (personAge >= 2 && personAge <= 6) {
+            System.out.println("Если возраст человека равен " + personAge + ", то ему нужно ходить в детский сад.");
+        } else if (personAge >= 7 && personAge <= 18) {
+            System.out.println("Если возраст человека равен " + personAge + ", то ему нужно ходить в школу.");
+        } else if (personAge > 18 && personAge <= 24) {
+            System.out.println("Если возраст человека равен " + personAge + ", то ему нужно ходить в университет.");
+        } else if (personAge > 24) {
+            System.out.println("Если возраст человека равен " + personAge + ", то ему нужно ходить на работу.");
+        }
+
+        // task 5
+        System.out.println("Задание 5");
+        int childAge = 14;
+        if (childAge < 5) {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему нельзя кататься на аттракционе.");
+        } else if (childAge >= 5 && childAge < 14) {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
+        } else if (childAge >= 14) {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься без сопровождения взрослого.");
+        }
+
+        // task 6
+        System.out.println("Задание 6");
+        int wagonPassengerCapacity = 102;
+        int wagonSittingPlace = 60;
+        int wagonStandingPlace = wagonPassengerCapacity - wagonSittingPlace; // => 42
+
+        int takenSittingPlace = 59;
+        int freeSittingPlace = wagonSittingPlace - takenSittingPlace;
+        int takenStandingPlace = 37;
+        int freeStandingPlace = wagonStandingPlace - takenStandingPlace;
+        int numTakenPlace = takenSittingPlace + takenStandingPlace;
+        int numFreePlace = wagonPassengerCapacity - numTakenPlace;
+
+        if (numTakenPlace == wagonPassengerCapacity) {
+            System.out.println("В вагоне больше нет мест!");
+        } else if (numTakenPlace > 102){
+            System.out.println("Ошибка! Пассажиров в вагоне больше допустимого значения");
+        } else {
+            System.out.println("Свободных мест в вагоне " + numFreePlace);
+        }
+
+        if (takenSittingPlace < 60 && numTakenPlace < wagonPassengerCapacity) {
+            System.out.println("Сидячих мест осталось " + freeSittingPlace);
+        } else if (takenSittingPlace == 60 && numTakenPlace < wagonPassengerCapacity) {
+            System.out.println("В вагоне больше нет сидячих мест!");
+        }
+
+        if (takenStandingPlace < 42 && numTakenPlace < wagonPassengerCapacity) {
+            System.out.println("Стоячих мест осталось " + freeStandingPlace);
+        } else if (takenStandingPlace == 42 && numTakenPlace < wagonPassengerCapacity) {
+            System.out.println("В вагоне больше нет стоячих мест!");
         }
     }
 }
